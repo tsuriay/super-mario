0d03ed2bb53d0909221396ad759dafd48beac4d8
Tsuriay <iamtsuriay@gmail.com>
2018-06-30 16:59:44 +0300

    lesson 13

--- public/js/TileCollider.js
+++ public/js/TileCollider.js
@@ -2,89 +2,77 @@ import TileResolver from './TileResolver.js';
 import {Sides} from './Entity.js';
 
 export default class TileCollider {
-    constructor(tileMatrix) {
-        this.tiles = new TileResolver(tileMatrix);
-    }
+	constructor(tileMatrix) {
+		this.tiles = new TileResolver(tileMatrix);
+	}
 
-    checkX(entity) {
-        let x;
+	checkX(entity) {
+		let x;
 
-        if (entity.vel.x > 0) {
-            x = entity.bounds.right;
-        } else if (entity.vel.x < 0) {
-            x = entity.bounds.left;
-        } else {
-            return;
-        }
+		if (entity.vel.x > 0) {
+			x = entity.bounds.right;
+		} else if (entity.vel.x < 0) {
+			x = entity.bounds.left;
+		} else {
+			return;
+		}
 
-        const matches = this.tiles.serchByRange(
-            x, x,
-            entity.bounds.top, entity.bounds.bottom
-        );
+		const matches = this.tiles.serchByRange(
+			x, x,
+			entity.bounds.top, entity.bounds.bottom
+		);
 
-        matches.forEach(match => {
-            if (match.tile.type !== 'ground') {
-                return;
-            }
-    
-            if (entity.vel.x > 0) {
-                if (entity.bounds.right > match.x1) {
-                    entity.bounds.left = match.x1 - entity.size.x;
-                    entity.vel.x = 0;
+		matches.forEach(match => {
+			if (match.tile.type !== 'ground') {
+				return;
+			}
+	
+			if (entity.vel.x > 0) {
+				if (entity.bounds.right > match.x1) {
+					entity.obstruct(Sides.RIGHT, match);
+				}
+			} else if (entity.vel.x < 0) {
+				if (entity.bounds.left < match.x2) {
+					entity.obstruct(Sides.LEFT, match);
+				}
+			}
+		});
+	}
 
-                    entity.obstruct(Sides.RIGHT);
-                }
-            } else if (entity.vel.x < 0) {
-                if (entity.bounds.left < match.x2) {
-                    entity.bounds.left = match.x2;
-                    entity.vel.x = 0;
+	checkY(entity) {
+		let y;
 
-                    entity.obstruct(Sides.LEFT);
-                }
-            }
-        });
-    }
+		if (entity.vel.y > 0) {
+			y = entity.bounds.bottom;
+		} else if (entity.vel.y < 0) {
+			y = entity.bounds.top;
+		} else {
+			return;
+		}
 
-    checkY(entity) {
-        let y;
+		const matches = this.tiles.serchByRange(
+			entity.bounds.left, entity.bounds.right,
+			y, y
+		);
 
-        if (entity.vel.y > 0) {
-            y = entity.bounds.bottom;
-        } else if (entity.vel.y < 0) {
-            y = entity.bounds.top;
-        } else {
-            return;
-        }
+		matches.forEach(match => {
+			if (match.tile.type !== 'ground') {
+				return;
+			}
+	
+			if (entity.vel.y > 0) {
+				if (entity.bounds.bottom > match.y1) {
+					entity.obstruct(Sides.BOTTOM, match);
+				}
+			} else if (entity.vel.y < 0) {
+				if (entity.bounds.top < match.y2) {
+					entity.obstruct(Sides.TOP, match);
+				}
+			}
+		});
+	}
 
-        const matches = this.tiles.serchByRange(
-            entity.bounds.left, entity.bounds.right,
-            y, y
-        );
-
-        matches.forEach(match => {
-            if (match.tile.type !== 'ground') {
-                return;
-            }
-    
-            if (entity.vel.y > 0) {
-                if (entity.bounds.bottom > match.y1) {
-                    entity.bounds.top = match.y1 - entity.size.y;
-                    entity.vel.y = 0;
-
-                    entity.obstruct(Sides.BOTTOM);
-                }
-            } else if (entity.vel.y < 0) {
-                if (entity.bounds.top < match.y2) {
-                    entity.bounds.top = match.y2;
-                    entity.vel.y = 0;
-
-                    entity.obstruct(Sides.TOP);
-                }
-            }
-        });
-    }
-
-    test(entity) {
-        this.checkY(entity);
-    }
+	test(entity) {
+		this.checkY(entity);
+	}
 }
--- public/js/entities/Koopa.js
+++ public/js/entities/Koopa.js
@@ -1,6 +1,8 @@
 import Entity, {Sides, Trait} from '../Entity.js';
 import PendulumMove from '../traits/PendulumMove.js';
 import Killable from '../traits/Killable.js';
+import Solid from '../traits/Solid.js';
+import Physics from '../traits/Physics.js';
 import {loadSpriteSheet} from '../loaders.js';
 
 const STATE_WALKING = Symbol('walking');
@@ -57,7 +59,7 @@ class Behaivor extends Trait {
 		} else if (this.state === STATE_HIDING) {
 			us.killable.kill();
 			us.vel.set(100, -200);
-			us.canCollide = false;
+			us.solid.obstructs = false;
 		} else if (this.state === STATE_PANIC) {
 			this.hide(us);
 		}
@@ -125,6 +127,8 @@ function createKoopaFactory(sprite) {
 		koopa.size.set(16, 16);
 		koopa.offset.y = +8;
 
+		koopa.addTrait(new Physics());
+		koopa.addTrait(new Solid());
 		koopa.addTrait(new PendulumMove());
 		koopa.addTrait(new Behaivor());
 		koopa.addTrait(new Killable());
