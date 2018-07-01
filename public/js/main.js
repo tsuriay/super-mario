eed249e81f35ec801a375131495545f64a3289ce
Tsuriay <iamtsuriay@gmail.com>
2018-07-01 17:42:18 +0300

    lesson 14

--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,10 @@ import Timer from './Timer.js';
 import Entity from './Entity.js';
 import PlayerController from './traits/PlayerController.js';
 import {createLevelLoader} from './loaders/level.js';
+import {loadFont} from './loaders/font.js';
 import {setupKeyboard} from './input.js';
-import {createCollisionLayer} from './layers.js';
+import {createCollisionLayer} from './layers/collision.js';
+import {createDashboardLayer} from './layers/dashboard.js';
 import {loadEntities} from './entities.js';
 
 function createPlayerEnv(playerEntity) {
@@ -20,7 +22,10 @@ function createPlayerEnv(playerEntity) {
 async function main(canvas) {
 	const context = canvas.getContext('2d');
 
-	const entityFactory = await loadEntities();
+	const [entityFactory, font] = await Promise.all([
+		loadEntities(),
+		loadFont()
+	]);
 	const loadLevel = await createLevelLoader(entityFactory);
 
 	const level = await loadLevel('1-1');
@@ -32,7 +37,8 @@ async function main(canvas) {
 	const playerEnv = createPlayerEnv(mario);
 	level.entities.add(playerEnv);
 
-	level.comp.layers.push(createCollisionLayer(level));
+	// level.comp.layers.push(createCollisionLayer(level)); // debug
+	level.comp.layers.push(createDashboardLayer(font, playerEnv));
 
 	const input = setupKeyboard(mario);
 	input.listenTo(window);
